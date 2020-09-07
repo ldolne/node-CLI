5940b98b3859b328e8486df7a812040016969e11
ldolne <dolnelaetitia@gmail.com>
2020-09-07 12:52:35 +0200

    Finish holidates exercise

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# node-CLI
+# `holidates` country
+## Presentation
+This was an assignment during my BeCode training in web development.
+
+The goal was to create a tool to get the holidays of the current year for the given country. 
+
+## Installation
+You can install it with npm:
+
+```
+npm install @ldolne/holidates
+```
+
+## Usage
+This module is a command line tool that will take a country name (mandatory) 
+and a year (optional, default is current year) as parameters:
+```
+holidates Belgium [2019]
+```
+
+This tool will verify that the parameter is a well-formatted country name, transform it onto a two-letters country code,
+then perform an HTTP request to the API of the [nager.date]("https://date.nager.at/") service to get the holidays for the chosen country and year.
+
+The results, which take the form of a list of holidays dates for the year selected, is showed in a readable way in the terminal.
+
+## Versioning
+* ...
+* Version 1.0.0: launch of the module
---
+++ index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+// Definitions of modules constants
+const axios = require('axios');
+const countryList = require('country-list');
+const chalk = require('chalk');
+const boxen = require('boxen');
+
+// Definitions of variables
+let args = process.argv.slice(2);
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let chosenYear;
+let isACountry = false;
+const allCountries = countryList.getNames();
+let countryCode;
+
+// Check if a second argument, the chosen year, has been given ; default is current year
+if(args[1] != null)
+{
+    chosenYear = args[1];
+}
+else
+{
+    chosenYear = currentYear;
+}
+
+// If chosen country exists, transform it onto a two-letters country code
+allCountries.forEach(element => {
+    if(args[0] == element)
+    {
+        isACountry = true;
+    }
+});
+
+if(isACountry)
+{
+    countryCode = countryList.getCode(args[0]);
+}
+else {
+    console.log("The country name given does not exist.\nPlease try again.");
+    return process.exit();
+}
+
+// HTTP request to the API of the nager.date service
+async function axiosGetHolidaysForOneCountry(year, countryCode) {
+    try {
+        return await axios.get("https://date.nager.at/api/v2/publicholidays/" + year + "/" + countryCode);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+axiosGetHolidaysForOneCountry(chosenYear, countryCode)
+.then(holidays => {
+    for(let i = 0; i < holidays['data'].length; i++) {
+        // Show the results (a list of holidays dates for the current year) in a readable way in the terminal
+        console.log(
+            chalk.green(
+                boxen(
+                    chalk.cyan.bold(holidays['data'][i]["date"]) + ": " + chalk.white(holidays['data'][i]["name"]),
+                    {
+                        padding: 1,
+                        margin: 0,
+                        borderStyle: "round"
+                    }
+                )
+            )
+        );
+    }
+})
+.catch(error => console.error(error));
+
+
